46fdfee9733c442cfcf06c028a769b003caaa8ef
TaylorReis-lab <taylor-reis@outlook.com>
2026-02-03 20:34:49 -0300

    feat: primeira versão da extensão

---
+++ blockList.js
@@ -0,0 +1,79 @@
+export const BLOCKED_SITES = [
+  // 🔞 Pornografia (principais)
+  "pornhub.com",
+  "xvideos.com",
+  "xnxx.com",
+  "redtube.com",
+  "youporn.com",
+  "tube8.com",
+  "spankbang.com",
+  "beeg.com",
+  "tnaflix.com",
+  "thumbzilla.com",
+  "pornmd.com",
+
+  // 🔞 Redes premium / estúdios
+  "brazzers.com",
+  "bangbros.com",
+  "realitykings.com",
+  "naughtyamerica.com",
+  "digitalplayground.com",
+  "twistys.com",
+  "fakehub.com",
+  "teamskeet.com",
+
+  // 🔞 Cams ao vivo
+  "chaturbate.com",
+  "stripchat.com",
+  "livejasmin.com",
+  "myfreecams.com",
+  "camsoda.com",
+  "bongacams.com",
+
+  // 🔞 Amador / agregadores
+  "eporner.com",
+  "pornone.com",
+  "porndoe.com",
+  "pornhd.com",
+  "xxxfiles.com",
+  "porn300.com",
+  "txxx.com",
+
+  // 🔞 Fetiche / nicho
+  "motherless.com",
+  "efukt.com",
+  "hclips.com",
+  "heavy-r.com",
+
+  // 🔞 Comunidades / fóruns
+  "pornbb.org",
+  "f95zone.to",
+  "leakedbb.com",
+  "coomer.su",
+  "kemono.su",
+
+  // 🔞 Criadores / conteúdo pago
+  "onlyfans.com",
+  "fansly.com",
+  "manyvids.com",
+  "justfor.fans",
+  "fanvue.com",
+
+  // 🔞 Hentai / animado
+  "hanime.tv",
+  "hentaihaven.xxx",
+  "nhentai.net",
+  "rule34.xxx",
+  "e-hentai.org",
+  "exhentai.org",
+
+  // 🔞 GIFs / imagens
+  "pornpics.com",
+  "sex.com",
+  "xhamster.com",
+
+  // 🔞 Buscadores e indexadores adultos
+  "pornmd.com",
+  "findtubes.com",
+  "pornzilla.com",
+];
